fd49df9cbc872974e0fa2f7d1fd1217e82b3667e
tga55a <tgardner2@my.waketech.edu>
2025-09-17 09:22:15 -0400

    Update NumberStairs.java

--- NumberStairs.java
+++ NumberStairs.java
@@ -1,7 +1,10 @@
+package Looping;
 public class NumberStairs {
     public static void main(String[] args) {
+        String Tower = "";
         for (int i = 1; i < 11; i++) {
-            System.out.print(i);
+            Tower = Tower + Integer.toString(i);
+            System.out.println(Tower);
         }
     }
 }
